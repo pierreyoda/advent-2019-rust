33ff68333af9b0d94b34ef4c7950b4f4ff28b5f6
pierreyoda <pierreyoda@users.noreply.github.com>
2020-11-20 09:20:23 +0100

    :recycle: Scaffolding inputs loading: from i64 to generic Signed number

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -2,11 +2,17 @@ use std::{
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
+    str::FromStr,
 };
 
 use anyhow::Result;
+use num_traits::Num;
 
-pub fn load_inputs_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<i64>> {
+pub fn load_inputs_from_file<N, P>(path: P) -> Result<Vec<N>>
+where
+    N: Copy + Num + Ord + FromStr,
+    P: AsRef<Path>,
+{
     let file = File::open(path)?;
     let lines = BufReader::new(file).lines();
     Ok(lines
