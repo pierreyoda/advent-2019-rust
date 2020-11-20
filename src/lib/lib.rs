181b15d356c9248987d37e90879d37e12f433f33
pierreyoda <pierreyoda@users.noreply.github.com>
2020-11-20 09:42:02 +0100

    :recycle: Scaffolding: add generic loading/computing function

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,8 +1,10 @@
 use std::{
+    fmt::Display,
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
     str::FromStr,
+    time::Instant,
 };
 
 use anyhow::Result;
@@ -21,4 +23,26 @@ where
         .map(|i| i.unwrap().parse())
         .filter_map(Result::ok)
         .collect())
+}
+
+pub fn run_with_scaffolding<N, F>(label: &'static str, compute: F) -> Result<N>
+where
+    N: Copy + Num + Ord + FromStr + Display,
+    F: Fn(Vec<N>) -> N,
+{
+    // Read input(s)
+    let input_start = Instant::now();
+    let input = load_inputs_from_file(format!("./src/{}/input.txt", label))?;
+    let input_time = input_start.elapsed();
+    println!("Inputs read in {:?}", input_time);
+
+    // Run computing function
+    let compute_start = Instant::now();
+    let output = compute(input);
+    let compute_time = compute_start.elapsed();
+    println!("Computing done in {:?}", compute_time);
+
+    // Output
+    println!("Result = {}", output);
+    Ok(output)
 }
